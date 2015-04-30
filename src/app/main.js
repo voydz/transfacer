de215ed6ababfffb4370c36969b3effa49f40908
Felix Rudat <frudat@doegel.de>
2015-04-30 15:10:15 +0200

    simple tests with settings

--- src/app/main.js
+++ src/app/main.js
@@ -1,6 +1,18 @@
+var path = require('path');
 var settings = require ('./modules/Settings');
 
-module.exports = function() {
-    settings.set('port', 1337);
-    console.log(settings.get('port'));
+module.exports = function(window, $) {
+
+    // simple test
+    setTheme($, settings.get('app.theme'));
 };
+
+function setTheme($, name)
+{
+    var base = '../node_modules/bootswatch';
+    var file = 'bootstrap.min.css';
+
+    $('link[data-theme]').attr('href', path.join(base, name, file));
+}
+
+
